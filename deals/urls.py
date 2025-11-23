2967475de2db7fc8fd5310217300772f092ac9be
Sewnarain Downeshwari <downeshwari.sewnarain@umail.uom.ac.mu>
2025-11-23 15:42:22 +0400

    Update urls.py

--- deals/urls.py
+++ deals/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+app_name = "deals" 
 
 urlpatterns = [
-    path('', views.deals, name='deals'),  # ✅ use the correct function name
+    path('', views.deals, name='deals'),  
 ]
