c4838dcbec20c8da821dcc62a3f835148bde20ad
Ritika Sewnarain <your_email@example.com>
2025-11-25 08:32:19 +0400

    Updated menu with no homepage

---
+++ Git-it-done/reviews/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Review(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.rating}/5)"
