c66b4b1f64fdef87ee1b1d2a04392dbb09bff3c7
Farhaanbg <muhammad.beegoo@umail.uom.ac.mu>
2025-11-26 18:10:36 +0400

    Updating reviews page

--- reviews/models.py
+++ reviews/models.py
@@ -8,3 +8,4 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.rating}/5)"
+
--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', views.reviews_page, name='reviews'),
+    path('', views.reviews_list, name='reviews'),  # Main reviews page (carousel of recent reviews)
+    path('add/', views.add_review, name='add_review'),  # Add a new review
+    path('all/', views.reviews_full_list, name='reviews_full_list'),  # Full list of reviews
 ]
